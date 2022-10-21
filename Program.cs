921d5af69dfaadcc4f68c759f43174ad67017e14
Matheus Carvalho <matheuso.c08@gmail.com>
2022-10-21 16:55:50 -0300

    Incluida novos objetos para teste

--- Program.cs
+++ Program.cs
@@ -5,9 +5,17 @@ WhiteWizard jenica = new WhiteWizard("Jenica");
 BlackWizard topapa = new BlackWizard("Topapa");
 Ninja wedge = new Ninja("Wedge");
 
+Knight cavaleiroArmado = new Knight("Cavaleiro Real", "Espada Sagrada", 100, 10);
+BlackWizard magoArcano = new BlackWizard("Mago Arcano", "Cajado Mistico", 40, 100);
 
-Console.WriteLine(arus);
-Console.WriteLine(jenica);
-Console.WriteLine(topapa);
-Console.WriteLine(wedge);
+Console.WriteLine(magoArcano);
+Console.WriteLine();
+Console.WriteLine(cavaleiroArmado);
+Console.WriteLine();
+
+Console.WriteLine(magoArcano.Attack());
+Console.WriteLine(magoArcano.Magic(8));
+
+Console.WriteLine(cavaleiroArmado.Attack());
+Console.WriteLine(cavaleiroArmado.Magic());
 
